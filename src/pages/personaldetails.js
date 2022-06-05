f9c789096f9fc3230c39f0a51e43b93d91df30df
sylvercodez <tosinotenaike3@gmail.com>
2022-06-05 20:06:33 +0100

    first commit

--- src/pages/personaldetails.js
+++ src/pages/personaldetails.js
@@ -11,32 +11,33 @@ import { Box, Button, Container, Grid, Link, TextField, Typography } from "@mui/
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Facebook as FacebookIcon } from "../icons/facebook";
 import { Google as GoogleIcon } from "../icons/google";
+import Layout from "src/components/Layout";
 
 const personaldetails = () => {
   const [value, setValue] = React.useState(0);
   const options = [
-    { value: "", label: "" },
+
     { value: "Euro", label: "Euro" },
     { value: "Naira", label: "Naira" },
     { value: "Dollar", label: "Dollar" },
   ];
   const weekdayOptions = [
-    { value: "", label: "" },
+
     { value: "Weekdays", label: "Weekdays" },
     { value: "Weekends", label: "Weekends" },
-    
+
   ];
   const cityOptions = [
-    { value: "", label: "" },
+
     { value: "Abuja", label: "Abuja" },
     { value: "Lagos", label: "Lagos" },
-    
+
   ];
   const countryOptions = [
-    { value: "", label: "" },
+
     { value: "Nigeria", label: "Nigeria" },
     { value: "India", label: "India" },
-    
+
   ];
   const customStyles = {
     indicatorSeparator: () => ({
@@ -96,36 +97,49 @@ const personaldetails = () => {
     },
   });
 
+  function proceed() {
+    console.log("Proceed");
+
+    // setSelectedBtns(document.querySelectorAll(".active"))
+  }
   return (
     <>
       <Head>
         <title>Enter Personal Details | Material Kit</title>
       </Head>
-      <div className="container ">
+      <Layout>
+      <div className="container " style={{marginTop:"70px", marginBottom:"100px"}}>
         <div className="login_wrapper">
           <div className="login personal_det">
             <h4>Complete your registration</h4>
             <span className="tab">1/3</span>
 
-            <div className="login_inputs">
+
               <form action="">
+              <div className="login_inputs">
                 <div className=" personal_det_inputs">
+                <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
                   <div className="login_input">
                     <label htmlFor="student_email">First name:</label> <br />
                     <div className="input_wrap">
                       <input type="text" />
                     </div>
                   </div>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
                   <div className="login_input">
                     <label htmlFor="student_email">Last name:</label> <br />
                     <div className="input_wrap">
                       <input type="text" />
                     </div>
                   </div>
+                </Grid>
+                    <Grid item xs={12} sm={6}>
                   <div className="login_input">
                     <label htmlFor="student_email">City:</label> <br />
                     <Select
-                        defaultValue={cityOptions[0]}
+
                         styles={customStyles}
                         options={cityOptions}
                         theme={(theme) => ({
@@ -140,34 +154,14 @@ const personaldetails = () => {
                           },
                         })}
                       />
-                    {/* <div className="input_wrap">
-                      <Select
-                        defaultValue={options[1]}
-                        styles={customStyles}
-                        options={options}
-                        theme={(theme) => ({
-                          ...theme,
 
-                          colors: {
-                            ...theme.colors,
-                            primary25: "#f2e9c4",
-                            primary: "#2f2e40",
-                            neutral5: "#000",
-                            primary50: "#f2e9c4",
-                          },
-                        })}
-                      />
-                      <select name="" id="">
-                        <option value="Naira"></option>
-                        <option value="Naira">Lagos</option>
-                        <option value="Naira">Abuja</option>
-                      </select>
-                    </div> */}
                   </div>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
                   <div className="login_input">
                     <label htmlFor="student_email">Country:</label> <br />
                     <Select
-                        defaultValue={countryOptions[0]}
+
                         styles={customStyles}
                         options={countryOptions}
                         theme={(theme) => ({
@@ -182,34 +176,14 @@ const personaldetails = () => {
                           },
                         })}
                       />
-                    {/* <div className="input_wrap">
-                      <Select
-                        defaultValue={options[1]}
-                        styles={customStyles}
-                        options={options}
-                        theme={(theme) => ({
-                          ...theme,
 
-                          colors: {
-                            ...theme.colors,
-                            primary25: "#f2e9c4",
-                            primary: "#2f2e40",
-                            neutral5: "#000",
-                            primary50: "#f2e9c4",
-                          },
-                        })}
-                      />
-                      <select name="" id="">
-                        <option value="Naira"></option>
-                        <option value="Naira">Nigeria</option>
-                        <option value="Naira">Japan</option>
-                      </select>
-                    </div> */}
                   </div>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
                   <div className="login_input">
                     <label htmlFor="student_email">When are you more available:</label> <br />
                     <Select
-                        defaultValue={weekdayOptions[0]}
+
                         styles={customStyles}
                         options={weekdayOptions}
                         theme={(theme) => ({
@@ -224,19 +198,14 @@ const personaldetails = () => {
                           },
                         })}
                       />
-                    {/* <div className="input_wrap">
-                      
-                      <select name="" id="">
-                        <option value="Naira"></option>
-                        <option value="Naira">Weekdays</option>
-                        <option value="Naira">Weekends</option>
-                      </select>
-                    </div> */}
+
                   </div>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
                   <div className="login_input">
                     <label htmlFor="student_email">Preferred Currency</label> <br />
                     <Select
-                        defaultValue={options[0]}
+
                         styles={customStyles}
                         options={options}
                         theme={(theme) => ({
@@ -251,26 +220,24 @@ const personaldetails = () => {
                           },
                         })}
                       />
-                    {/* <div className="input_wrap">
-                      
-                      <select name="" id="">
-                        <option value="Naira"></option>
-                        <option value="Naira">Naira</option>
-                        <option value="Naira">Euro</option>
-                      </select>
-                    </div> */}
-                  </div>
+
+                </div>
+                </Grid>
+                </Grid>
+                </div>
+                </div>
+                <div className="login_btn" style={{display:"flex",justifyContent:"center"}}>
+                <button onClick={proceed}  className="actn_btn">
+                    Proceed
+                  </button>
                 </div>
 
-                <div className="login_btn">
-                  <button type="submit">Proceed</button>
-                </div>
               </form>
-            </div>
+
           </div>
         </div>
       </div>
-
+      </Layout>
     </>
   );
 };
